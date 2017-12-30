46f7eb3c8947aae1ce80a3c743fe1794a9767ea7
Marko Cupic <m.cupic@gmx.ch>
2017-12-30 18:31:32 +0100

    dev

--- src/Controller/BackendPasswordRecoveryController.php
+++ src/Controller/BackendPasswordRecoveryController.php
@@ -12,57 +12,35 @@
 
 namespace Markocupic\BackendPasswordRecoveryBundle\Controller;
 
-use Contao\BackendAlerts;
-use Contao\BackendConfirm;
-use Contao\BackendFile;
-use Contao\BackendHelp;
-use Contao\BackendIndex;
+
 use Contao\BackendMain;
-use Contao\BackendPage;
-use Contao\BackendPassword;
-use Contao\BackendPopup;
-use Contao\BackendPreview;
-use Contao\BackendSwitch;
-use Contao\CoreBundle\Picker\PickerConfig;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Markocupic\BackendPasswordRecoveryBundle\BackendPassword;
 
 /**
  * Class BackendPasswordRecoveryController
  * @package Markocupic\BackendPasswordRecovery\Controller
- * @Route(defaults={"_scope" = "backend"})
  */
 class BackendPasswordRecoveryController extends Controller
 {
 
-    /**
-     * Handles backendpasswordrecovery requests.
-     * @return JsonResponse
-     * @Route("/backendpasswordrecovery", name="backend_password_recovery")
-     */
-    public function sendlinkAction()
-    {
-        $this->container->get('contao.framework')->initialize();
 
-        $controller = new BackendMain();
-
-        return $controller->run();
-    }
 
     /**
      * @return Response
      *
-     * @Route("/backendpasswordrecovery/enterpassword", name="backend_password_recovery")
+     * @Route("/backendpasswordrecovery/{slug}", name="backend_password_recovery")
      */
-    public function mainAction()
+    public function mainAction($slug)
     {
         $this->container->get('contao.framework')->initialize();
 
-        $controller = new BackendMain();
+        $controller = new BackendPassword($slug);
 
         return $controller->run();
     }
